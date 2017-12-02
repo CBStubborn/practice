d76da26a66b95f295b058bd8d473e50e85a9397f
CBStubborn <CBStubborn@163.com>
2017-12-02 11:09:50 +0800

    resolve conflict in .idea/workspace.xml

--- personal-kafka/src/main/java/personal/practices/kafka/jnproject/MessageProducer.java
+++ personal-kafka/src/main/java/personal/practices/kafka/jnproject/MessageProducer.java
@@ -1,10 +1,18 @@
 package personal.practices.kafka.jnproject;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 public class MessageProducer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MessageProducer.class);
 
     private String defaultConfFile = "conf/kafka/kafka-producer.properties";
 
@@ -17,8 +25,19 @@ public class MessageProducer {
     }
 
     public void init() {
-        File file = new File(defaultConfFile);
-        Properties properties = new Properties();
+        FileReader file = null;
+        try {
+            file = new FileReader(defaultConfFile);
+            Properties properties = new Properties();
+            properties.load(file);
+            producerConfig = new ProducerConfig(properties);
+        } catch (FileNotFoundException e) {
+            LOG.error("Cannot found the file: [{}], please check it!", defaultConfFile);
+            System.exit(1);
+        } catch (IOException e) {
+            LOG.error("IOException while read the file: [{}]", defaultConfFile);
+        }
+
 
     }
 }
